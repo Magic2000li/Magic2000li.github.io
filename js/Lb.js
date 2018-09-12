cf9a8f701a1f38aeb97f6cf0157b926a819350e7
Yang <2281074196@qq.com>
2018-09-12 10:50:49 +0800

    fade

--- js/Lb.js
+++ js/Lb.js
@@ -1,69 +1,82 @@
 /**
  * Created by Science2050 on 2017/2/15.
  */
+;(function (window,undefend) {
 
-var tab=document.getElementById("tab");
-var bo=document.getElementById("bo");
-var l=document.getElementById("l");
-var r=document.getElementById("r");
-var t=bo.getElementsByTagName("img");
-var h=tab.getElementsByTagName("li");
+    var tab=document.getElementById("tab");
+    var bo=document.getElementById("bo");
+    var l=document.getElementById("l");
+    var r=document.getElementById("r");
+    var t=bo.getElementsByTagName("img");
+    var h=tab.getElementsByTagName("li");
 
 // 鼠标滑过变
-function dong(o) {
-    for ( var i=0;i<h.length;i++){
-        h[i].className="";
-        t[i].className=""
+    function dong(o) {
+        for ( var i=0;i<h.length;i++){
+            h[i].className="";
+            t[i].className=""
+        }
+        h[o].className="ok";
+        t[o].className="ok"
     }
-    h[o].className="ok";
-    t[o].className="ok"
-}
+    var flag=null;//标记
+    var timer=null;
+    var de=0;
 
-for(var j=0;j<h.length;j++){
-    h[j].index=j;
-    h[j].onmouseover=function () {
-        dong(this.index)
-    }
-
-}
-// 自动
-var timer=null;
-var de=0;
-
-function run() {
-    // clearInterval(timer);
-    timer=setInterval(function () {
-        de++;
-        if(de>=h.length){
-            de=0;
+// 点击切换
+    for(var j=0;j<h.length;j++){
+        h[j].index=j;
+        h[j].onmouseover=function () {
+            dong(this.index);
+            flag=this.index
         }
 
-        dong(de)
-    },3000);
-
-}
-run();
-// 鼠标移入
-bo.onmouseover=function () {
-    clearInterval(timer)
-};
-// 鼠标移出
-bo.onmouseout=function () {
-    run();
-};
+    }
 // 左按钮
-l.onclick=function () {
-    de-=1;
-    if(de<0){
-        de=h.length-1
-    }
-    dong(de)
-};
+    l.onclick=function () {
+        if(typeof flag !=='undefind'){
+            de=flag;
+            flag--;
+            if(flag<0){
+                flag=h.length-1
+            }
+        }
+        de-=1;
+        if(de<0){
+            de=h.length-1
+        }
+        dong(de)
+    };
 // 右按钮
-r.onclick=function () {
-    de++;
-    if(de>=h.length){
-        de=0
+
+    r.onclick=function () {
+        if( typeof flag!=='undefind'){
+            de=flag;
+            flag++;
+            if(flag>h.length-1){
+                flag=0;
+            }
+        }
+        de++;
+        if(de>h.length-1){
+            de=0
+        }
+        dong(de)
+    };
+// 自动
+    function run() {
+        clearInterval(timer);
+        timer=setInterval(r.onclick,3000);
+
     }
-    dong(de)
-};
+    run();
+// 鼠标移入
+    bo.onmouseover=function () {
+        clearInterval(timer)
+    };
+// 鼠标移出
+    bo.onmouseout=function () {
+        run();
+    };
+
+})(window);
